b2f75d04c4eca94e7e41351ce583b3474200d125
edgar929 <shumbushoedgar@gmail.com>
2020-11-24 23:45:30 +0200

    added trycatch in dbConnection

--- Db/dbConnection.js
+++ Db/dbConnection.js
@@ -3,14 +3,19 @@ require('dotenv').config()
 const connectionURL =process.env.dbConnection
 
 const connectDb = async()=>{
-    await mongoose.connect(connectionURL,{
-        // useMongoClient:true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    });
-    console.log('connected')
+    try {
+        await mongoose.connect(connectionURL,{
+            // useMongoClient:true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        });
+        console.log('connected')
+    } catch (error) {
+        
+    }
+   
 }
 
 module.exports = connectDb;
